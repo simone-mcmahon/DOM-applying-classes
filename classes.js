c60c997910218c2ba3aeccea9cfd8ef98cf4935c
simone-mcmahon <simoneabdust@gmail.com>
2020-02-23 19:49:05 +1300

    make fourth div visible

--- classes.js
+++ classes.js
@@ -1,25 +1,41 @@
 // Don't change or delete this line! It waits until the DOM has loaded, then calls 
 // the start function. More info: 
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
-document.addEventListener('DOMContentLoaded', start)
+document.addEventListener('DOMContentLoaded', start);
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-   one()
-    
+   one();
+   two();
+   three();
+   makeVisible();
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
 function one () {
   // First, we have to find the element:
-  var one = document.getElementById('one')
+  var one = document.getElementById('one');
 
   // Next, we apply a new CSS class to it:
-  one.classList.add('blue')
+  one.classList.add('blue');
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two');
 
+  two.classList.add('yellow');
+}
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three');
+
+  three.classList.add('red');
+}
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible () {
+  var invisible = document.getElementsByClassName('invisible')[0];
+
+  invisible.classList.add('visible');
+}
